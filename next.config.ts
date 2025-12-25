d841372639fa217f2df1523ae056b83957588307
iambuka <b.gulua@bet25.com>
2025-12-25 19:50:32 +0400

    modify config file

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // ✅ REQUIRED for static hosting
+  output: "export",
+
+  // Optional but recommended for static sites
+  trailingSlash: true,
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'bet25.com',
+        protocol: "https",
+        hostname: "bet25.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.figma.com',
+        protocol: "https",
+        hostname: "www.figma.com",
       },
     ],
   },
